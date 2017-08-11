dddae04d54103739da09915b98ec5d22d9abd7ff
xiaoziqingren <1045977704@qq.com>
2017-08-11 09:21:32 +0800

    添加插入排序 #1
    添加插入排序

--- main.go
+++ main.go
@@ -14,6 +14,12 @@ func main() {
 	fmt.Println(sorts.BubbleSortOne(testSlice))
 	fmt.Println(sorts.BubbleSortTwo(testSlice))
 
+	// 选择排序
+	fmt.Println(sorts.SelectSort(testSlice))
+
+	// 插入排序
+	fmt.Println(sorts.InsertSort(testSlice))
+
 	// 切片追加、插入、删除操作
 	fmt.Println(gosnippet.SliceHandle(testSlice, 8, 1))
 }
