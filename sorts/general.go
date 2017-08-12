29f77356cc080f731f4e2adf43d61d9053b74cf2
xiaoziqingren <1045977704@qq.com>
2017-08-12 16:04:19 +0800

    规范代码格式
    规范代码格式

--- sorts/general.go
+++ sorts/general.go
@@ -3,7 +3,7 @@ package sorts
 // 检测是否有序
 func IsSorted(previous []int) bool {
 	for i := 1; i < len(previous); i++ {
-		if previous[i] < previous[i - 1] {
+		if previous[i] < previous[i-1] {
 			return false
 		}
 	}
--- sorts/insertsort.go
+++ sorts/insertsort.go
@@ -3,8 +3,8 @@ package sorts
 // 插入排序：从第二个开始，每次都和前面的比较排序，总保证前面的是有序的
 func InsertSort(previous []int) []int {
 	for i := 1; i < len(previous); i++ {
-		for j := i; j > 0 && previous[j] < previous[j - 1]; j-- {
-			previous[j], previous[j - 1] = previous[j - 1], previous[j]
+		for j := i; j > 0 && previous[j] < previous[j-1]; j-- {
+			previous[j], previous[j-1] = previous[j-1], previous[j]
 		}
 	}
 
