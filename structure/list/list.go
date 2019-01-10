ade44b680e565036e834cc72ef5416f484e30a3b
sunanxiang <1045977704@qq.com>
2019-01-10 21:44:06 +0800

    add reverse singly linked list

---
+++ structure/list/list.go
@@ -0,0 +1,51 @@
+package list
+
+// singly linked list
+type SinglyNode struct {
+	Next  *SinglyNode
+	Value int
+}
+
+// two-way linked list
+type TwoWayNode struct {
+	Next  *TwoWayNode
+	Pre   *TwoWayNode
+	Value int
+}
+
+// reverse singly linked list use non-recursive way
+func ReverseSinglyLinkedListNonRecursive(root *SinglyNode) *SinglyNode {
+	if root == nil || root.Next == nil {
+		return root
+	}
+
+	var preNode, nextNode *SinglyNode
+
+	for root.Next != nil {
+		nextNode = root.Next
+		root.Next = preNode
+		preNode = root
+		root = nextNode
+	}
+	return preNode
+}
+
+// reverse singly linked list use recursive way
+func ReverseSinglyLinkedListRecursive(root *SinglyNode) *SinglyNode {
+	if root == nil || root.Next == nil {
+		return root
+	}
+
+	var nextNode *SinglyNode
+	nextNode = root.Next
+	root.Next = nil
+	reverse := ReverseSinglyLinkedListRecursive(nextNode)
+	nextNode.Next = root
+	return reverse
+}
+
+// todo:
+// reverse two-way linked list
+func ReverseTwoWayLinkedList(root *TwoWayNode) *TwoWayNode {
+	return nil
+}
